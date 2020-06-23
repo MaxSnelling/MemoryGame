41702b7f4800b2b25b208303b552216fda12c0a3
maxsnelling <max.snelling@outlook.com>
2020-06-23 20:01:40 +0100

    Added menu and game page with navigation

---
+++ app/src/main/java/com/maxsnelling/memorygame/GameFragment.kt
@@ -0,0 +1,24 @@
+package com.maxsnelling.memorygame
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
